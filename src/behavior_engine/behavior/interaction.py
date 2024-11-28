a8bfdeeca5b147afb5177f94fd2fddbd3d8bccbf
kmorrisongr <mckinleybagel@pm.me>
2024-11-28 16:40:07 -0500

    protocol should not implement anything

--- src/behavior_engine/behavior/interaction.py
+++ src/behavior_engine/behavior/interaction.py
@@ -6,11 +6,6 @@ from behavior_engine.simulation.state import WorldState
 
 
 class Interaction[T: Actor](Protocol):
-    actor_cls: type[T]
-
-    def get_actor(self, actor_name: UUID, state: WorldState) -> T:
-        return state.get_entity(self.actor_cls, actor_name)
-
     def __call__(
         self,
         actor_name: UUID,
@@ -21,11 +16,6 @@ class Interaction[T: Actor](Protocol):
 
 
 class SelfInteraction[T: Actor](Protocol):
-    actor_cls: type[T]
-
-    def get_actor(self, actor_name: UUID, state: WorldState) -> T:
-        return state.get_entity(self.actor_cls, actor_name)
-
     def __call__(
         self,
         actor_name: UUID,
