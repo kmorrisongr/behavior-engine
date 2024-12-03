1e315b98c04ba5cd2dc96c39fcbab79da485e832
kmorrisongr <mckinleybagel@pm.me>
2024-12-03 15:47:32 -0500

    remove duplicated code

--- src/behavior_engine/behavior/interaction/__init__.py
+++ src/behavior_engine/behavior/interaction/__init__.py
@@ -1,4 +1,4 @@
-from typing import Protocol, TypeVar
+from typing import Protocol
 from uuid import UUID
 
 from behavior_engine.model.actor import Actor
@@ -38,28 +38,3 @@ class InteractionCreator[T: Actor](Protocol):
 
 class SelfInteractionCreator[T: Actor](Protocol):
     def __call__(self, call_cls: type[SelfInteractionCall]) -> type[SelfInteraction[T]]: ...
-
-
-T = TypeVar("T", bound=Actor, covariant=True)
-
-
-def InteractionFactory(actor_cls: type[T]) -> InteractionCreator[T]:
-    def decorator(call_cls: type[InteractionCall]) -> type[Interaction[T]]:
-        def get_actor(cls: type[object], actor_name: UUID, state: WorldState) -> T:
-            return state.get_entity(actor_cls, actor_name)
-
-        setattr(call_cls, "get_actor", classmethod(get_actor))
-        return call_cls  # pyright: ignore[reportReturnType]
-
-    return decorator
-
-
-def SelfInteractionFactory(actor_cls: type[T]) -> SelfInteractionCreator[T]:
-    def decorator(call_cls: type[SelfInteractionCall]) -> type[SelfInteraction[T]]:
-        def get_actor(cls: type[object], actor_name: UUID, state: WorldState) -> T:
-            return state.get_entity(actor_cls, actor_name)
-
-        setattr(call_cls, "get_actor", classmethod(get_actor))
-        return call_cls  # pyright: ignore[reportReturnType]
-
-    return decorator
